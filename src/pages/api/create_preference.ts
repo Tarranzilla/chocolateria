2671d3a99b9b071428097733e911c572abb6a1be
JOAO TARRAN <joaotarran@gmail.com>
2024-04-24 15:03:02 -0300

    order ID generator first try

--- src/pages/api/create_preference.ts
+++ src/pages/api/create_preference.ts
@@ -1,11 +1,41 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import admin, { ServiceAccount } from "firebase-admin";
+import axios, { AxiosError } from "axios";
+
+if (!process.env.FIREBASE_PRIVATE_KEY) {
+    throw new Error("The FIREBASE_PRIVATE_KEY environment variable is not defined");
+}
+
+const serviceAccount: admin.ServiceAccount = {
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+};
+
 // SDK do Mercado Pago
 import { MercadoPagoConfig, Preference } from "mercadopago";
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://pragmatas-dev.firebaseio.com",
+});
+
+function generateOrderID() {
+    const firestore = admin.firestore();
+    const projectUID = "WIlxTvYLd20rFopeFTZT"; // Replace with your project's UID
+    const ordersCollectionRef = firestore.collection(`projects/${projectUID}/orders`);
+    const ordersCount = ordersCollectionRef.count();
+
+    console.log("ordersCount:", ordersCount.query.count);
+    console.log("ordersCount query.count:", ordersCount.query.count);
+}
+
 // Adicione as credenciais
 const client = new MercadoPagoConfig({ accessToken: "TEST-5866109528270009-031110-5662bc4f630f78b04519adecead1b55b-201803820" });
 
-export default function preferenceHandler(req, res) {
+export default function preferenceHandler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === "POST") {
-        let cartItems = req.body.map((item) => ({
+        let cartItems = req.body.map((item: any) => ({
             title: item.id,
             unit_price: item.price,
             quantity: item.quantity,
@@ -13,6 +43,8 @@ export default function preferenceHandler(req, res) {
 
         // console.log(cartItems);
         const preference = new Preference(client);
+
+        generateOrderID();
 
         preference
             .create({
